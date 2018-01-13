db9e454d72abd25b62603712a8361be7213428be
VinodPc <vinodpb@outlook.com>
2018-01-13 05:30:24 -0500

    Add drawe

--- src/App.js
+++ src/App.js
@@ -1,21 +1,39 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import AppBar from 'material-ui/AppBar'
+import logo from './logo.svg'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import './App.css'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  handleClose = () => this.setState({open: false});
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+       <AppBar
+        title="Title"
+         iconClassNameRight="muidocs-icon-navigation-expand-more"
+         onLeftIconButtonClick ={this.handleToggle}
+       />
+        <Drawer
+          docked={false}
+          width={200}
+          open={this.state.open}
+          onRequestChange={(open) => this.setState({open})}
+        >
+          <MenuItem onClick={this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onClick={this.handleClose}>Menu Item 2</MenuItem>
+        </Drawer>
+    </div>
     );
   }
 }
 
-export default App;
+export default App
